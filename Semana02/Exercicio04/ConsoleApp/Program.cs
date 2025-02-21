b2f30b519612e5f5281729f487057040477035d9
nathanbcampos <nathanbcampos@gmail.com>
2025-02-20 21:17:45 -0300

    update

--- Semana02/Exercicio04/ConsoleApp/Program.cs
+++ Semana02/Exercicio04/ConsoleApp/Program.cs
@@ -2,8 +2,10 @@
 int numberOfBillGates = 1;
 string name = "Nathan";
 var myaccount = 19600000000000;
+var charchar = 'c';
 
 Console.WriteLine(billGates);
 Console.WriteLine(numberOfBillGates);
 Console.WriteLine(name);
 Console.WriteLine(myaccount);
+Console.WriteLine(charchar);
--- Semana02/Exercicio04/ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
+++ Semana02/Exercicio04/ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38bceb6dcf55d6fa8d5706efe5286fa0bb863a0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+169833a895d8f57ac51e3a5080083342891c3ea6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
