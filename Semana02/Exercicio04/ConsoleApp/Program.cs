686c94f3a99e5a91acfdcd159f82b740da56ac6a
nathanbcampos <nathanbcampos@gmail.com>
2025-02-21 15:17:06 -0300

    video 04 string c#

--- Semana02/Exercicio04/ConsoleApp/Program.cs
+++ Semana02/Exercicio04/ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
-﻿//Create REad Update Delete
+﻿using System;
+using System.Text;
+
+//Create REad Update Delete
 //CRUD
 //Create
 
@@ -7,4 +10,21 @@ string petFish = "puffer fish";
 //Concatenation
 petFish = "porcupine " + petFish;
 
-Console.Write(petFish);
+//Template Literal
+/*Console.WriteLine($"I AM BUYING {petFish}. YOU CANNOT STOP ME!!!");*/
+
+/*Console.WriteLine(petFish);*/
+
+//Read
+//Console.Writeline()
+
+//Update
+string newPetFish = petFish.Replace("porcupine", "bluer dot");
+Console.WriteLine(newPetFish);
+
+//DELETE 
+StringBuilder newCrustacen = new StringBuilder();
+newCrustacen.Append("fiddler crab");
+Console.WriteLine(newCrustacen);
+newCrustacen.Remove(0, 8);
+Console.WriteLine(newCrustacen);
