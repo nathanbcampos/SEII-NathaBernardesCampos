b738c352545e55ddec2f13033a9f3cc9f26ef37b
nathanbcampos <nathanbcampos@gmail.com>
2025-02-21 14:54:18 -0300

    update

--- Semana02/Exercicio04/ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
+++ Semana02/Exercicio04/ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+169833a895d8f57ac51e3a5080083342891c3ea6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2f30b519612e5f5281729f487057040477035d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
