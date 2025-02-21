43bb9ff55395b31157a7dfc8582dc12b588e7e32
nathanbcampos <nathanbcampos@gmail.com>
2025-02-21 19:43:36 -0300

    video 07 if else  c#

--- Semana02/Exercicio04/ConsoleApp/Program.cs
+++ Semana02/Exercicio04/ConsoleApp/Program.cs
@@ -1,20 +1,14 @@
-﻿//Counter
-//
-/*for(var i = 0; i < 7; i++){*/
-/*	Console.WriteLine(i);*/
-/*}*/
-
-string[] professores = ["Alberto", "Carlos", "Roberto"];
-
-/*for(var i = 0; i < nomes.Length; i++){*/
-/*	Console.WriteLine(nomes[i]);*/
-/*}*/
-
-
-foreach(var aulas in professores){
-	Console.WriteLine(aulas);
+﻿
+bool Estudou = false;
+bool notaBoa = false;
+if ((Estudou) && (notaBoa))
+{
+	Console.WriteLine("Parabens voce estudou, e foi recompensado por isso");
+}
+else if((Estudou) && (!notaBoa)) {
+	Console.WriteLine("Voce estudou mas não foi suficiente para sair bem na prova");
+}
+else if((!Estudou) && (!notaBoa)){
+	Console.WriteLine("Teve o que merece");
 }
 
-professores.ToList().ForEach((i) => {
-	Console.WriteLine("ForEach: " + i);
-});
