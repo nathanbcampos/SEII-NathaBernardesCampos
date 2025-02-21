ff129b9243f2a2d5af0c70fb595a321e02c3c591
nathanbcampos <nathanbcampos@gmail.com>
2025-02-21 19:52:49 -0300

    video 08 enum  c#

--- Semana02/Exercicio04/ConsoleApp/Program.cs
+++ Semana02/Exercicio04/ConsoleApp/Program.cs
@@ -1,14 +1,15 @@
 ﻿
-bool Estudou = false;
-bool notaBoa = false;
-if ((Estudou) && (notaBoa))
+
+
+var status = Warning.CodeYellow;
+
+if (status == Warning.CodeYellow)
 {
-	Console.WriteLine("Parabens voce estudou, e foi recompensado por isso");
-}
-else if((Estudou) && (!notaBoa)) {
-	Console.WriteLine("Voce estudou mas não foi suficiente para sair bem na prova");
-}
-else if((!Estudou) && (!notaBoa)){
-	Console.WriteLine("Teve o que merece");
+	Console.WriteLine("CODE YELLOW");
 }
 
+enum Warning{
+	CodeRed,
+	CodeBlue,
+	CodeYellow
+}
