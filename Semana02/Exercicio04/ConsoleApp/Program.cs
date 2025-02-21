169833a895d8f57ac51e3a5080083342891c3ea6
nathanbcampos <nathanbcampos@gmail.com>
2025-02-20 21:16:17 -0300

    update

--- Semana02/Exercicio04/ConsoleApp/Program.cs
+++ Semana02/Exercicio04/ConsoleApp/Program.cs
@@ -1,5 +1,9 @@
 ﻿string billGates = "sai fora BILL GATES!";
 int numberOfBillGates = 1;
+string name = "Nathan";
+var myaccount = 19600000000000;
 
 Console.WriteLine(billGates);
 Console.WriteLine(numberOfBillGates);
+Console.WriteLine(name);
+Console.WriteLine(myaccount);
