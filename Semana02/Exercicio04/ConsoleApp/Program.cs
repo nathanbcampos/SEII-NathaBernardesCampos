922e7ecfc07a17dfa63d465f10270991acd95057
nathanbcampos <nathanbcampos@gmail.com>
2025-02-22 15:56:44 -0300

    video 12 Methods  c#

--- Semana02/Exercicio04/ConsoleApp/Program.cs
+++ Semana02/Exercicio04/ConsoleApp/Program.cs
@@ -1,32 +1,10 @@
-﻿using ConsoleApp; 
+﻿using ConsoleApp;
 
-Rat wolfRat = new Rat();
-wolfRat.Name = "Mestre Splinter";
-wolfRat.Number = 30;
-wolfRat.IsRadioactive = true;
+RealState elmStreet = new()
+{
+	Address = "Elm Street",
+	SquareFootage = 1300,
+	Price = 300000
+};
 
-
-Console.WriteLine(wolfRat.Name);
-Console.WriteLine(wolfRat.Number);
-Console.WriteLine(wolfRat.IsRadioactive);
-
-Console.WriteLine("---------------------");
-
-Rat fancyRat = new Rat();
-fancyRat.Name = "Leoncio";
-fancyRat.Number = 10;
-fancyRat.IsRadioactive = false;
-
-Console.WriteLine(fancyRat.Name);
-Console.WriteLine(fancyRat.Number);
-Console.WriteLine(fancyRat.IsRadioactive);
-
-Console.WriteLine("---------------------");
-
-//Update
-wolfRat.Name = "Wolf Rat";
-Console.WriteLine(wolfRat.Name);
-
-//Delete
-wolfRat.Name = "";
-Console.WriteLine(wolfRat.Name);
+Console.WriteLine(elmStreet.CalculatePricePerSquareFoot()); 
