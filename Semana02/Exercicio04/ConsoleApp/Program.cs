b21cf886367dd550115abdee27f2dc65c0b7bf93
nathanbcampos <nathanbcampos@gmail.com>
2025-02-22 22:37:47 -0300

    video 19 Delegates, Func & Action  c#

--- Semana02/Exercicio04/ConsoleApp/Program.cs
+++ Semana02/Exercicio04/ConsoleApp/Program.cs
@@ -1,21 +1,20 @@
-﻿
-Person person1 = new("Marco", "Silva", 30, new string[1]  {"9022-3842"});
-Person person2 = new("Marco", "Silva", 30, new string[1]  {"9022-3842"});
-/*Person person2 = new("Pedro", "Oliveira", 23,new string[1]  {"3234-4234"});*/
+﻿Action loggerAction = () =>
+{
+    Console.WriteLine("This is simple");
+};
 
-//Immutability 
-/*person1.LastName = "silva";*/
-/*person2.PhoneNumber[0] = "0000-4230";*/
+Func<int, int> loggerFunc = (int x) => 
+{
+    return x+2;
+};
 
-//Equality
-//value
-//Referential
-Console.WriteLine(person1 == person2);
+Console.WriteLine(loggerFunc(3));
 
-//Non-destructive (copy)
-Person person3 = person2 with {LastName = "Brewski"};
-Console.WriteLine(person3.ToString());
+var list = Enumerable.Range(1,10).Select(i => i*3).ToList();
 
-public record Person(string FirstName, string LastName, int Age, string[] PhoneNumber);
+var callCall = (Action func) =>
+{
+    func();
+};
 
-
+callCall(loggerAction);
--- Semana02/Exercicio04/ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
+++ Semana02/Exercicio04/ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+887ea16ca88e650a7a8cf83129bdcb74bc7bff63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13adec98bda98324ef48582bc982e29d12909e89")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
