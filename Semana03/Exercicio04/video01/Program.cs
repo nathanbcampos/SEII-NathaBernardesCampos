d8906f8cc4b60286c18536df06e876aea9b1c0e9
nathanbcampos <nathanbcampos@gmail.com>
2025-03-06 22:42:44 -0300

    added semana03/exercicio04

---
+++ Semana03/Exercicio04/video01/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AlgorithmDataStructure;
+
+pokemon squirtle = new pokemon();
+squirtle.name = "Squirtle";
+
+Console.WriteLine(squirtle.name);
+
---
+++ Semana03/Exercicio04/video01/pokemon.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmDataStructure
+{
+	public class pokemon
+	{
+		public string? name { get; set;}
+		public int Exp {get ; set; }
+
+		public bool isPokemon()
+		{
+			return true;
+		}
+	}
+}
