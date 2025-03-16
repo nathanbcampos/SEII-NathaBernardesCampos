f5df24190b209fa9fbaff8949bc388ae552fa3f8
nathanbcampos <nathanbcampos@gmail.com>
2025-03-15 22:14:21 -0300

    added semana03/exercicio4/video4

---
+++ Semana03/Exercicio04/video04/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] intArray = new int[9];
+
+int lenght = 0;
+/*Preenche os array*/
+for(int i = 0; i < 6; i++)
+{
+	intArray[lenght] = i;
+	lenght++;
+}
+
+
+/*Deleta o ultimo array*/
+/*lenght--;*/
+
+/*Deleta o primeiro array*/
+/*for (int i = 1; i < lenght; i++)*/
+/*	intArray[i - 1] = intArray[i];*/
+
+/*Deleta o array do meio */
+for (int i = 4; i < lenght; i++)
+	intArray[i - 1] = intArray[i];
+
+lenght--;
+
+
+for(int i =0; i < lenght; i++)
+	Console.WriteLine(intArray[i]);
