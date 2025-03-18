28090e57daaaf84f2ca261020e6a688a117bfc6b
nathanbcampos <nathanbcampos@gmail.com>
2025-03-18 20:20:27 -0300

    added semana03/exercicio4/video6

---
+++ Semana03/Exercicio04/video06/Program.cs
@@ -0,0 +1,31 @@
+﻿using AlgorithmsDataStructure.LinkedList;
+
+/*Node nodeA = new Node();*/
+/*nodeA.Data = 3;*/
+/**/
+/*Node nodeB = new Node();*/
+/*nodeB.Data = 344;*/
+/**/
+/*Node nodeC = new Node();*/
+/*nodeC.Data = 2397;*/
+/**/
+/*Node nodeD = new Node();*/
+/*nodeD.Data = 321;*/
+/**/
+/*nodeA.Next = nodeB;*/
+/*nodeB.Next = nodeC;*/
+/*nodeC.Next = nodeD;*/
+
+LinkedList linkedlist = new LinkedList();
+linkedlist.InsertFirst(1);
+linkedlist.InsertFirst(2);
+linkedlist.InsertFirst(3);
+linkedlist.InsertFirst(4);
+
+linkedlist.DeleteFirst();
+linkedlist.DeleteFirst();
+
+linkedlist.InsertLast(546);
+linkedlist.InsertLast(3434);
+
+linkedlist.DisplayList();
