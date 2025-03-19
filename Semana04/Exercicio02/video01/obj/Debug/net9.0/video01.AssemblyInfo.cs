e1d5064d1415693bf62a46503dd901a13aad7ffb
nathanbcampos <nathanbcampos@gmail.com>
2025-03-18 21:20:43 -0300

    added semana04/exercicio02/video01

--- Semana04/Exercicio02/video01/obj/Debug/net9.0/video01.AssemblyInfo.cs
+++ Semana04/Exercicio02/video01/obj/Debug/net9.0/video01.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("video07")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("video01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c69166b51d07807b07de5ba40294e2ebd6b145e5")]
-[assembly: System.Reflection.AssemblyProductAttribute("video07")]
-[assembly: System.Reflection.AssemblyTitleAttribute("video07")]
+[assembly: System.Reflection.AssemblyProductAttribute("video01")]
+[assembly: System.Reflection.AssemblyTitleAttribute("video01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
