37c08d7ead036b0494c425a554a2d284de39b20d
nathanbcampos <nathanbcampos@gmail.com>
2025-03-20 20:43:20 -0300

    added semana04/exercicio02/video07

---
+++ Semana04/Exercicio02/video07/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace vide07Construtor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pessoa a = new Pessoa();
+			Pessoa b = new Pessoa("Roberto");
+		}
+	}
+}
