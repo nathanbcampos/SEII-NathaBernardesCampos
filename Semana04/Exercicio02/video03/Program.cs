72bdcdae8f7a208c1f0fa65e2c2bb41b4e316992
nathanbcampos <nathanbcampos@gmail.com>
2025-03-19 22:28:42 -0300

    added semana04/exercicio02/video03

---
+++ Semana04/Exercicio02/video03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Program
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Instanciar objeto da classe Aluno
+			Aluno a = new Aluno();
+			a.nome = "Roberto";
+			a.nota1 = 4;
+			a.nota2 = 5;
+			a.mensagem();
+		}
+	}
+}
