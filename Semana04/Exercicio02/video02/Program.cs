221a4798d8a0dd318450966ee20efddccdfd73aa
nathanbcampos <nathanbcampos@gmail.com>
2025-03-19 22:18:27 -0300

    added semana04/exercicio02/video02

---
+++ Semana04/Exercicio02/video02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01Conceitos
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Instanciar um objeto
+			Pessoa obj = new Pessoa();
+			obj.apresentar("Ralf",30);
+		}
+	}
+}
