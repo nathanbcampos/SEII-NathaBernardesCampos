3b083d59db4f4c272998b4628088d85ab1076079
nathanbcampos <nathanbcampos@gmail.com>
2025-03-20 22:02:00 -0300

    added semana04/exercicio02/video12

---
+++ Semana04/Exercicio02/video12/Exemplo.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Exemplo
+{
+	// Metodo estatico
+	public static void soma(int n1, int n2)
+	{
+		Console.WriteLine(n1+n2);
+	}
+}
---
+++ Semana04/Exercicio02/video12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace video12
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Exemplo.soma(10,12);
+		}
+	}
+}
