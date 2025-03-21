3e4ae7230a70b98d67ebd08f20d19a50903fb78e
nathanbcampos <nathanbcampos@gmail.com>
2025-03-20 22:21:18 -0300

    added semana04/exercicio02/video14

---
+++ Semana04/Exercicio02/video14/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace video14
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Calculo c = new Calculo();
+			c.somar(10,15);
+			c.subtrair(50,18);
+		}
+	}
+}
