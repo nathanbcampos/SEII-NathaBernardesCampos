f4f17b4461fad0517bbf5077d786d1fae4e898f9
nathanbcampos <nathanbcampos@gmail.com>
2025-03-24 13:58:03 -0300

    added semana05/exercicio03/video01

---
+++ Semana05/Exercicio03/video01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+class MainClass
+{
+	static void Main()
+	{
+		var thread = new Thread(DifferentMethod);
+		thread.Start();
+	}
+
+	static void DifferentMethod()
+	{
+		Console.WriteLine("My Name");
+	}
+}
