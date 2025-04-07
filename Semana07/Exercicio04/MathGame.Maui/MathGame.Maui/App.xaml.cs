5ed34e09a557d4c9fc88686f0c9228fabc084ee0
nathanbcampos <nathanbcampos@gmail.com>
2025-04-06 23:12:40 -0300

    added semana07/exercicio04

---
+++ Semana07/Exercicio04/MathGame.Maui/MathGame.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MathGame.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
