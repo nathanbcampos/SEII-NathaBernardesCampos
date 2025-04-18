e4c35ba18305ef770febafd33e5911443b31accc
nathanbcampos <nathanbcampos@gmail.com>
2025-04-18 19:46:40 -0300

    added Semana09/Exercicio02

---
+++ Semana09/Exercicio02/Projeto03/MyTodoApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyTodoApp.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Todo> Todos {get; set;}
+}
